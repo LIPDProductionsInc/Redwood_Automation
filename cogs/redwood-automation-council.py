059f6522216aea1c6ed53438001afee6fa9a0279
LIPDProductionsInc <willy4rules@gmail.com>
2022-10-10 02:14:49 -0400

    Updated redwood-automation-council

--- cogs/redwood-automation-council.py
+++ cogs/redwood-automation-council.py
@@ -1,6 +1,9 @@
+from random import choices
+from typing import Literal
 import discord
 import datetime
 
+from discord import app_commands
 from discord.ext import commands
 from datetime import timedelta
 
@@ -18,11 +21,25 @@ class CouncilCog(commands.Cog, name="Council Commands Cog"):
             )
         embed.add_field(name="Mayor", value={member.mention for member in ctx.guild.members if discord.utils.get(member.roles, id=646549322682466305)}, inline=True)
         embed.add_field(name="Deputy Mayor", value={member.mention for member in ctx.guild.members if discord.utils.get(member.roles, id=646551227626160139)}, inline=True)
-        embed.add_field(name="Council Chairperson", value={member.mention for member in ctx.guild.members if discord.utils.get(member.roles, id=673008336010084378)}, inline=False)
-        embed.add_field(name="Council Members", value="\n".join([member.mention for member in ctx.guild.members if discord.utils.get(member.roles, id=646549329493884929)]), inline=True)
+        embed.add_field(name="City Council Chairperson", value={member.mention for member in ctx.guild.members if discord.utils.get(member.roles, id=673008336010084378)}, inline=False)
+        embed.add_field(name="City Council Members", value="\n".join([member.mention for member in ctx.guild.members if discord.utils.get(member.roles, id=646549329493884929)]), inline=True)
         embed.set_footer(text=f"Redwood Automation | Developed by {self.bot.owner} | Information Accurate As Of:", icon_url=self.bot.avatar_url)
         embed.timestamp = datetime.datetime.now()
         await ctx.send(embed=embed)
+        pass
+
+    @app_commands.command(name="docket", description="Has the bot announce the next item on the city council docket.")
+    @commands.guild_only()
+    @commands.has_any_role(646549322682466305, 646551227626160139, 673008336010084378)
+    
+    async def docket(self, ctx, first: Literal[True, False], docket_item, docket_link):
+        """If the item is the first item of the session, the bot will announce as such. Otherwise, it will announce the next item on the docket."""
+        #if ctx.category_id == 646552329654370345:
+        if first == True:
+            await ctx.send(f"The first item on the docket is {docket_item}. \n\n{docket_link} \n Floor is open for debate. Say \"I\" to be recognized. (<@&646549329493884929>)")
+        else:
+            await ctx.send(f"The next item on the docket is {docket_item}. \n\n{docket_link} \n Floor is open for debate. Say \"I\" to be recognized. (<@&646549329493884929>)")
+            pass
         pass
 
     pass
