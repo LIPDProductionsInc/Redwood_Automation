bf22249daa09fe429110d27f2bd610c4636cae0a
LIPDProductionsInc <willy4rules@gmail.com>
2022-11-02 09:54:12 -0400

    New command

--- cogs/redwood-automation-council.py
+++ cogs/redwood-automation-council.py
@@ -204,6 +204,18 @@ class CouncilCog(commands.Cog, name="Council Commands Cog"):
             pass
         pass
 
+    @commands.hybrid_command(name="vote", description="Has the City Council start a vote")
+    @commands.guild_only()
+    @commands.has_any_role(646549322682466305, 646551227626160139, 673008336010084378)
+    @app_commands.describe(type="The type of vote to start.")
+    async def vote(self, ctx, type:Literal["Amendment", "Bill", "Resolution", "Motion"]):
+        if ctx.channel.name.startswith("council-session"):
+            await ctx.send(f"Vote on the {type.lower()}\n\n<@&646549329493884929>")
+        else:
+            raise commands.UserInputError("This command can only be used in a council session channel.")
+            pass
+        pass
+
     @commands.hybrid_command(name="charter", description="Sends a link to the City Charter.")
     @commands.guild_only()
     async def charter(self, ctx):
