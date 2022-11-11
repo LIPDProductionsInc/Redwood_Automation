d9728a91bd2d1c977bf852c4e3eeb0fd08455f79
LIPDProductionsInc <n00846739@gmail.com>
2022-11-11 11:01:10 -0500

    Doesn't Work with Context Menus, Needs More Testing

--- cogs/redwood-automation-context.py
+++ cogs/redwood-automation-context.py
@@ -1,24 +1,7 @@
 import discord
-import traceback
 
 from discord import app_commands
 from discord.ext import commands
-
-class TestModal(discord.ui.Modal, title="Title"):
-    feedback = discord.ui.TextInput(
-        label='Label',
-        style=discord.TextStyle.long,
-        placeholder='Placeholder',
-        required=False,
-        max_length=300,
-    )
-
-    async def on_submit(self, interaction: discord.Interaction):
-        await interaction.response.send_message(f'Thanks for your feedback, {self.name.value}!')
-
-    async def on_error(self, interaction: discord.Interaction, error: Exception) -> None:
-        await interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
-        traceback.print_tb(error.__traceback__)
 
 class ContextTestCog(commands.Cog, name="Context Test Cog"):
     def __init__(self, bot: commands.Bot) -> None:
@@ -26,10 +9,11 @@ class ContextTestCog(commands.Cog, name="Context Test Cog"):
         self.bot.tree.add_command(app_commands.ContextMenu(name='Context Test', callback=self.context_menu_callback))
 
     async def context_menu_callback(self, interaction: discord.Interaction, message: discord.Message) -> None:
-        await interaction.response.send_modal(TestModal())
+        await interaction.response.sendmessage('Context Test', ephemeral=True)
+        await message.reply('Reply')
         pass
 
     pass
 
 async def setup(bot: commands.Bot) -> None:
-    bot.add_cog(ContextTestCog(bot))
+    await bot.add_cog(ContextTestCog(bot))
