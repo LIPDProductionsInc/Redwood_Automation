66e48db7aff07aecf428312c23c841dc3394cccf
LIPDProductionsInc <n00846739@gmail.com>
2022-11-15 21:54:19 -0500

    Part of Another Repo. Testing now (Waiting to figure out on where to install it)

--- cogs/redwood-automation-clerk.py
+++ cogs/redwood-automation-clerk.py
@@ -1,8 +1,17 @@
 import discord
-import asyncio
+import os
+import subprocess
 
 from discord import app_commands
 from discord.ext import commands
+
+def get_chat_exporter_path():
+    if os.name == 'nt': # windows environment
+        return f'.{os.sep}DiscordChatExporter.CLI{os.sep}DiscordChatExporter.Cli.exe'
+    elif os.name == 'posix': # linux environment
+        return f'dotnet .{os.sep}DiscordChatExporter.CLI{os.sep}DiscordChatExporter.Cli.dll'
+    else:
+        return
 
 class ClerkCog(commands.Cog, name="Clerk Commands"):
     def __init__(self, bot: commands.Bot) -> None:
@@ -10,7 +19,8 @@ class ClerkCog(commands.Cog, name="Clerk Commands"):
 
     @commands.hybrid_command(name='transcript', description='Get a transcript of the session')
     @commands.guild_only()
-    @commands.has_role(763471193524535336)
+    @commands.check_any(commands.is_owner(), commands.has_role(763471193524535336))
+    #@commands.has_role(763471193524535336)
     async def transcript(self, ctx: commands.Context) -> None:
         if ctx.channel.name.startswith("council-session"):
             await ctx.send("`Saving...`")
@@ -24,41 +34,19 @@ class ClerkCog(commands.Cog, name="Clerk Commands"):
             raise commands.UserInputError("This command can only be used in a council session channel.")
         pass
 
-    @commands.hybrid_command(name='transcript-colorful', description='Get a transcript of the session (Colorful Version)')
+    @commands.hybrid_command(name='transcript-simple', description='Get a transcript of the session (Simple Version)')
     @commands.guild_only()
     @commands.is_owner()
     #@commands.has_role(763471193524535336)
-    async def transcript(self, ctx: commands.Context) -> None:
+    async def transcript_colorful(self, ctx: commands.Context) -> None:
         if ctx.channel.name.startswith("council-session"):
             await ctx.send("`Saving...`")
-            transcript = "<!DOCTYPE html>\n<html lang=\"en\">\n<head></head>\n<body>\n"
-            transcript += f"<div class=\"session_preamble\">\n<div class=\"session_preamble_guild_icon\>\n<img src=\"{ctx.guild.icon}\" alt=\"Guild Icon\">\n</div>\n"
-            transcript += f"<div class=\"session_preamble_entries-container\">\n<div id=\"guild_name\">{ctx.guild.name}</div>\n"
-            transcript += f"<div id=\"session_name\">{ctx.channel.name}</div>\n"
-            count = 0
-            async for message in ctx.channel.history(limit=None):
-                count += 1
-                transcript += f"<div class=\"total_message_count\">Messages: {count}</div>\n</div>\n</div>\n"
-            transcript += "<div id=\"chatlog\" class=\"chatlog\">\n"
-            async for message in ctx.channel.history(limit=None, oldest_first=True):
-                transcript += "<div class=\"chatlog_message_group\">\n"
-                transcript += f"<div class=\"chatlog_author-avatar-container\">\n<img class=\"chatlog_author-avatar\" src=\"{message.author.avatar}\" alt=\"Avatar\">\n</div>\n"
-                transcript += f"<div class=\"chatlog_messages\">\n<span class=\"chatlog_author-name\" title=\"{message.author.name}\" data-user-id=\"{message.author.id}\" style=\"color: {message.author.color}\">{message.author.display_name}[{message.author.id}]</span>\n"
-                transcript += f"<span class=\"chatlog_timestamp\">{message.created_at}</span>\n"
-                transcript += f"<div id=\"message - {message.id}\" class=\"chatlog_message\" data-message-id=\"{message.id}\ title=Message sent: {message.created_at}\">\n"
-                transcript += f"<div class=\"chatlog_content\">\n<div class=\"markdown\"><span class=\"preserve-whitespace\">\n{message.content}</span>\n</div>\n</div>\n</div>\n"
-                if message.embeds:
-                    for embed in message.embeds:
-                        transcript += f"<div class=\"chatlog_embed\">\n<div class=\"chatlog_embed-content-container\">\n<div class=\"chatlog_embed-content\">\n"
-                        transcript += f"<div class=\"chatlog_embed-text\">\n<div class=\"chatlog_embed-title\">\n"
-                        transcript += f"<a class=\"chatlog_embed-title-link\" href=\"{embed.url}\"><div class=\"markdown preserve-whitespace\">{embed.title}\n</div></a>\n"
-                        transcript += f"<div class=\"chatlog_embed-description\">\n<div class=\"markdown preserve-whitespace\">{embed.description}\n</div>"
-                        transcript += f"</div>\n</div>\n</div>\n</div>\n</div>\n"
-                transcript += "</div>\n</div>\n</div>\n"
-            transcript += "</div>\n</body>\n</html>"
-            with open("transcript.html", "w") as file:
-                file.write(transcript)
-            await ctx.send(file=discord.File("transcript.html", filename=f"{ctx.channel.name}-transcript.html"))
+            path = get_chat_exporter_path()
+            if not path:
+                raise commands.CommandError("Unsupported OS")
+            file_path = f"{ctx.channel.name}-transcript.html"
+            subprocess.run([path, "export", "-t", "html", "-c", "redwood", "-o", file_path, "-f", "html", "-b", "0", "-e", "0", "-i", ctx.channel.id])
+            await ctx.send(file=discord.File(file_path, filename=file_path))
         else:
             raise commands.UserInputError("This command can only be used in a council session channel.")
         pass
