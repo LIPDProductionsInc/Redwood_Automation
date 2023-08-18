1aed380be7ee2b4c62ec099c050d7ff89876bb9f
LIPDProductionsInc <n00846739@gmail.com>
2023-08-18 17:02:13 -0400

    Transfer Applications

--- cogs/redwood-automation-pd.py
+++ cogs/redwood-automation-pd.py
@@ -12,6 +12,8 @@ open_description = """Are you ready to make a difference? The Redwood Police Dep
 
 Join our ranks and become a part of a team that values diversity, teamwork, and continuous growth. As a Redwood police officer, you'll receive comprehensive training, access to cutting-edge resources, and the chance to build lasting connections within our community."""
 
+transfer_description = "Attention law enforcement professionals! Exciting news: transfer applications are now open for those seeking to bring their skills and dedication to the Redwood Police Department. Take the next step in your career journey and become an integral part of our dynamic team. Apply today and be a force for positive change in our community. Your experience matters - let's make a difference together at RPD!"
+
 class RedwoodAutomationPD(commands.Cog, name="Police Commands"):
     def __init__(self, bot) -> None:
         self.bot = bot
@@ -36,6 +38,28 @@ class RedwoodAutomationPD(commands.Cog, name="Police Commands"):
                 message = await city_channel.send(embed=embed)
                 await message.publish()
                 await ctx.send("Opened applications. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
+            elif status == "Transfer Only":
+                embed = discord.Embed(
+                    title="Redwood Police Department General Applications",
+                    colour=discord.Color(0xbb7000),
+                    type="rich",
+                    description="The Redwood Police Department do not currently have applications open to the general public."
+                )
+                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
+                embed.set_image(url="https://cdn.discordapp.com/attachments/1047644039870152794/1137541647534661762/Screenshot_99.png")
+                embed2 = discord.Embed(
+                    title="Redwood Police Department Transfer Applications",
+                    colour=discord.Color(0x007713),
+                    type="rich",
+                    description=transfer_description
+                )
+                embed2.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
+                embed2.set_image(url="https://cdn.discordapp.com/attachments/975458359501271070/1135769455364935771/Screenshot_66.png")
+                await channel.send(embed=embed)
+                await channel.send(embed=embed2)
+                message = await city_channel.send(embed=embed2)
+                await message.publish()
+                await ctx.send("Transfer Applications opened. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
 
     pass
 
