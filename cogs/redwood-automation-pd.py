4fbaa287fbbd330f96b15fb13ef544317c945cef
LIPDProductionsInc <n00846739@gmail.com>
2023-08-18 02:42:36 -0400

    Application Command Init

--- cogs/redwood-automation-pd.py
+++ cogs/redwood-automation-pd.py
@@ -1,13 +1,22 @@
 import discord
 import datetime
+import typing
 
 from discord.ext import commands
+from discord import app_commands
+from typing import Literal
+
+#Descriptions
 
 class RedwoodAutomationPD(commands.Cog, name="Police Commands"):
     def __init__(self, bot) -> None:
         self.bot = bot
     
-    #Commands
+    @commands.hybrid_command(name="applications", description="Opens and closes applications for the Redwood Police Department.")
+    @commands.has_role(1005949022248378469)
+    @app_commands.describe(status="Open or Close applications")
+    async def applications(self, ctx: commands.Context, status: Literal["Open", "Transfer Only" "Close", "Init"]) -> None:
+        if ctx.guild.id == 1005182438265335901:
 
     pass
 
