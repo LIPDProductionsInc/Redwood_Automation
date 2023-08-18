af50d7fa207bd7a9c87973a5121e14bff7c4a316
LIPDProductionsInc <n00846739@gmail.com>
2023-08-18 02:42:49 -0400

    Open Argument

--- cogs/redwood-automation-pd.py
+++ cogs/redwood-automation-pd.py
@@ -8,6 +8,10 @@ from typing import Literal
 
 #Descriptions
 
+open_description = """Are you ready to make a difference? The Redwood Police Department is excited to announce that we are now accepting applications for dedicated individuals who are passionate about upholding the values of safety, integrity, and community.
+
+Join our ranks and become a part of a team that values diversity, teamwork, and continuous growth. As a Redwood police officer, you'll receive comprehensive training, access to cutting-edge resources, and the chance to build lasting connections within our community."""
+
 class RedwoodAutomationPD(commands.Cog, name="Police Commands"):
     def __init__(self, bot) -> None:
         self.bot = bot
@@ -17,6 +21,20 @@ class RedwoodAutomationPD(commands.Cog, name="Police Commands"):
     @app_commands.describe(status="Open or Close applications")
     async def applications(self, ctx: commands.Context, status: Literal["Open", "Transfer Only" "Close", "Init"]) -> None:
         if ctx.guild.id == 1005182438265335901:
+            if status == "Open":
+                channel = ctx.bot.get_channel(1026530495569346590)
+                city_channel = ctx.bot.get_channel(646541531523710996)
+                embed = discord.Embed(
+                    title="Redwood Police Department Applications are OPEN!",
+                    colour=discord.Color(0x007713),
+                    type="rich",
+                    description=open_description
+                )
+                embed.set_footer(text="As a reminder, a valid POST certification is required")
+                await channel.send(embed=embed)
+                message = await city_channel.send(embed=embed)
+                await message.publish()
+                await ctx.send("Opened applications. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
 
     pass
 
