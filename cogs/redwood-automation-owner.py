25a46f5d69437f35a95c66405d8fba4b8b341ef7
LIPDProductionsInc <n00846739@gmail.com>
2023-08-29 05:39:35 -0400

    Sync Fix

--- cogs/redwood-automation-owner.py
+++ cogs/redwood-automation-owner.py
@@ -100,8 +100,10 @@ class OwnerCog(commands.Cog, name="Owner Commands"):
         await ctx.send('`Syncing Slash commands...`')
         print('Syncing slash commands')
         synced = await ctx.bot.tree.sync()
-        await ctx.send(f"`Synced {len(synced)} commands`")
-        print(f"Synced {len(synced)} commands")
+        city_synced = await ctx.bot.tree.sync(guild=646540220539338773)
+        pd_synced = await ctx.bot.tree.sync(guild=1005182438265335901)
+        await ctx.send(f"`Synced {len(synced)} commands globally, {len(city_synced)} commands in the City of Redwood guild, and {len(pd_synced)} commands in the Redwood Police Department guild`")
+        print(f"Synced {len(synced)} commands globally, {len(city_synced)} commands in the City of Redwood guild, and {len(pd_synced)} commands in the Redwood Police Department guild")
         return
 
     
