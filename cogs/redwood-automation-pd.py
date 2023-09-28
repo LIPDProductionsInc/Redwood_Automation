7e41de74c98c93b67c971fedf1d7da5b75761264
LIPDProductionsInc <n00846739@gmail.com>
2023-09-28 08:40:50 -0400

    Sync to PD Server Only

--- cogs/redwood-automation-pd.py
+++ cogs/redwood-automation-pd.py
@@ -39,76 +39,74 @@ class RedwoodAutomationPD(commands.Cog, name="Police Commands"):
     def __init__(self, bot) -> None:
         self.bot = bot
     
-    @commands.hybrid_command(name="applications", description="Opens and closes applications for the Redwood Police Department.")
-    @commands.has_role(1005949022248378469)
+    @app_commands.command(name="applications", description="Opens and closes applications for the Redwood Police Department.")
+    @app_commands.checks.has_role(1005949022248378469)
+    @app_commands.guilds(1005182438265335901)
     @app_commands.describe(status="Open or Close applications")
     async def applications(self, ctx: commands.Context, status: Literal["Open", "Transfer Only" "Close", "Init"]) -> None:
-        if ctx.guild.id == 1005182438265335901:
-            channel = ctx.bot.get_channel(1026530495569346590)
-            city_channel = ctx.bot.get_channel(646541531523710996)
-            if status == "Open":
-                embed = discord.Embed(
-                    title="Redwood Police Department Applications are OPEN!",
-                    colour=discord.Color(0x007713),
-                    type="rich",
-                    description=open_description
-                )
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
-                embed.set_footer(text="As a reminder, a valid POST certification is required")
-                await channel.send(embed=embed)
-                message = await city_channel.send(embed=embed)
-                await message.publish()
-                await ctx.send("Opened applications. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
-            elif status == "Transfer Only":
-                embed = discord.Embed(
-                    title="Redwood Police Department General Applications",
-                    colour=discord.Color(0xbb7000),
-                    type="rich",
-                    description="The Redwood Police Department do not currently have applications open to the general public."
-                )
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
-                embed.set_image(url="https://cdn.discordapp.com/attachments/1047644039870152794/1137541647534661762/Screenshot_99.png")
-                embed2 = discord.Embed(
-                    title="Redwood Police Department Transfer Applications",
-                    colour=discord.Color(0x007713),
-                    type="rich",
-                    description=transfer_description
-                )
-                embed2.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
-                embed2.set_image(url="https://cdn.discordapp.com/attachments/975458359501271070/1135769455364935771/Screenshot_66.png")
-                await channel.send(embed=embed)
-                await channel.send(embed=embed2)
-                message = await city_channel.send(embed=embed2)
-                await message.publish()
-                await ctx.send("Transfer Applications opened. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
-            elif status == "Close":
-                embed = discord.Embed(
-                    title="Redwood Police Department General Applications",
-                    colour=discord.Color(0x920707),
-                    type="rich",
-                    description="The Redwood Police Department regrets to inform you that our application process is currently closed. Please keep an eye on this channel for updates on when we have opened them up once again."
-                )
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
-                embed.set_image(url="https://cdn.discordapp.com/attachments/1047644039870152794/1140329049303040050/Screenshot_143.png")
-                await channel.send(embed=embed)
-                message = await city_channel.send(embed=embed)
-                await message.publish()
-                await ctx.send("Closed applications. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
-            elif status == "Init":
-                embed = discord.Embed(
-                    title="The Redwood Police Department",
-                    colour=discord.Color(0x004272),
-                    type="rich",
-                    description=init_description
-                )
-                embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
-                embed.set_image(url="https://cdn.discordapp.com/attachments/561690984803598346/1135257342158585978/image.png")
-                await channel.send(embed=embed)
-                await ctx.send("Initialized.")
-            else:
-                raise commands.BadArgument("Invalid status.")
+        channel = ctx.bot.get_channel(1026530495569346590)
+        city_channel = ctx.bot.get_channel(646541531523710996)
+        if status == "Open":
+            embed = discord.Embed(
+                title="Redwood Police Department Applications are OPEN!",
+                colour=discord.Color(0x007713),
+                type="rich",
+                description=open_description
+            )
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
+            embed.set_footer(text="As a reminder, a valid POST certification is required")
+            await channel.send(embed=embed)
+            message = await city_channel.send(embed=embed)
+            await message.publish()
+            await ctx.send("Opened applications. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
+        elif status == "Transfer Only":
+            embed = discord.Embed(
+                title="Redwood Police Department General Applications",
+                colour=discord.Color(0xbb7000),
+                type="rich",
+                description="The Redwood Police Department do not currently have applications open to the general public."
+            )
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1047644039870152794/1137541647534661762/Screenshot_99.png")
+            embed2 = discord.Embed(
+                title="Redwood Police Department Transfer Applications",
+                colour=discord.Color(0x007713),
+                type="rich",
+                description=transfer_description
+            )
+            embed2.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
+            embed2.set_image(url="https://cdn.discordapp.com/attachments/975458359501271070/1135769455364935771/Screenshot_66.png")
+            await channel.send(embed=embed)
+            await channel.send(embed=embed2)
+            message = await city_channel.send(embed=embed2)
+            await message.publish()
+            await ctx.send("Transfer Applications opened. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
+        elif status == "Close":
+            embed = discord.Embed(
+                title="Redwood Police Department General Applications",
+                colour=discord.Color(0x920707),
+                type="rich",
+                description="The Redwood Police Department regrets to inform you that our application process is currently closed. Please keep an eye on this channel for updates on when we have opened them up once again."
+            )
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
+            embed.set_image(url="https://cdn.discordapp.com/attachments/1047644039870152794/1140329049303040050/Screenshot_143.png")
+            await channel.send(embed=embed)
+            message = await city_channel.send(embed=embed)
+            await message.publish()
+            await ctx.send("Closed applications. Remember to delete any embeds in <#1026530495569346590> that say otherwise.", ephemeral=True)
+        elif status == "Init":
+            embed = discord.Embed(
+                title="The Redwood Police Department",
+                colour=discord.Color(0x004272),
+                type="rich",
+                description=init_description
+            )
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1041839113000726558/1142198375291289640/RPD_Seal.png")
+            embed.set_image(url="https://cdn.discordapp.com/attachments/561690984803598346/1135257342158585978/image.png")
+            await channel.send(embed=embed)
+            await ctx.send("Initialized.")
         else:
-            raise commands.UserInputError("This command can only be used in the RPD server.")
+            raise commands.BadArgument("Invalid status.")
         pass
 
     @commands.hybrid_command(name="mdt", description="Get the MDT")
