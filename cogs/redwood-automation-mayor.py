27e90d7fbca8c98356d9b09af1e6ee7c129a98a0
LIPDProductionsInc <n00846739@gmail.com>
2025-06-11 02:08:59 -0400

    Local Guild Command Attempt

--- cogs/redwood-automation-mayor.py
+++ cogs/redwood-automation-mayor.py
@@ -47,6 +47,7 @@ class MayorCog(commands.Cog, name="Mayor Commands"):
         pass
 
     @commands.hybrid_command(name="oaths", description="Begins the oath taking process for a new term")
+    @app_commands.guilds(discord.Object(id=1150770058847588492)) # City of Redwood
     @commands.has_any_role(1150770058935681163, 1150770058935681162, 1150770058935681160, 1150770058914705534, 1154217793030471721) # County Executive, Mayor, Deputy Mayor, Council Chairperson
     @app_commands.describe(term_id="The ID of the term (Ie, '2023-3')")
     async def oaths(self, ctx:commands.Context, term_id:str) -> None:
@@ -68,6 +69,7 @@ class MayorCog(commands.Cog, name="Mayor Commands"):
         pass
 
     @commands.hybrid_command(name="seal", description="Updates the seal of the city")
+    @app_commands.guilds(discord.Object(id=1150770058847588492)) # City of Redwood
     @commands.guild_only()
     @commands.check_any(commands.is_owner(), commands.has_any_role(1150770058897920159, 1150770058935681160, 1150770058935681160)) # Press Secretary, Deputy Mayor, Mayor
     @app_commands.describe(file="The PNG file to use as the seal")
