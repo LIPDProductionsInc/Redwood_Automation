02ed4b37f6e22af494f724fdb08bc3de3b31a88a
LIPDProductionsInc <n00846739@gmail.com>
2025-06-26 14:30:16 -0400

    Fix Category ID And Add Overwrites Back

--- cogs/redwood-automation-council.py
+++ cogs/redwood-automation-council.py
@@ -98,8 +98,8 @@ class CouncilCog(commands.Cog, name="Council Commands Cog"):
                     ctx.guild.get_role(1150770058847588500): discord.PermissionOverwrite(view_channel=True, send_messages=False), # Redwood Citizens
                     ctx.guild.default_role: discord.PermissionOverwrite(view_channel=False, add_reactions=False, send_messages=False) # @everyone
                 }
-                await ctx.channel.edit(category=ctx.guild.get_channel(761730715024097311), reason="Session Ended", position=0)
                 await channel.send(f"<@&1150770058897920157>\n\nHi, the session in {ctx.channel.mention} has been adjourned and is awaiting transcribing!")
+                await ctx.channel.edit(category=ctx.guild.get_channel(1150770063411003425), reason="Session Ended", overwrites=overwrites, position=0)
             else:
                 if ctx.interaction == None:
                     await ctx.message.delete()
